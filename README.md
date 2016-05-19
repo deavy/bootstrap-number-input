64a467e0e73752fd5d23cac74cded0dfd8a5f8ee
deavy <deavyinc@gmail.com>
2016-05-19 22:11:22 +0300

    Plugin refactoring
    Complete plugin refactoring

--- README.md
+++ README.md
@@ -1,15 +1,37 @@
 ![Screenshot](screenshot.png "bootstrap-spin.js")
 
-Very simple, fast and lightweight (> 3K) jquery plugin for bootstrap to render number input more user-friendly.
+Very simple, fast and lightweight (> 3K) jQuery plugin with Bootstrap support for number input more user-friendly.
+This is an improved version of a wpic's [bootstrap-number-input](https://github.com/wpic/bootstrap-number-input).
 
 ## Usage
 
+Add `input` element with `type="number` into your HTML-code with `value`, `min`, `max` and `step` (optional) attributes:
+
+```html
+<input class="form-control" type="number" value="1" min="1" max="10" />
+```
+
+Initialize plugin with
+
 ```javascript
-$(SELECTOR).bootstrapNumber({
-	upClass: success/primary/danger/warning/default,
-	downClass: success/primary/danger/warning/default,
-	center: true/false
+$(selector).bootstrapNumber({
+	center: true/false,
+	upClass: 'class-name' (success/primary/danger/warning/default),
+	downClass: 'class-name' (success/primary/danger/warning/default),
+	useStaticMarkup: true/false
 });
 ```
 
-Checkout here for [demo](http://wpic.bitbucket.org/bootstrap-number-input/sample.html)
+The `useStaticMarkup` option allows you to use generated plugin's markdown instead of simple `input`-element.
+
+```html
+<div class="input-group">
+    <span class="input-group-btn">
+        <button type="button" class="btn btn-danger">-</button>
+    </span>
+    <input id="colorful" class="form-control" type="text" value="1" min="1" max="10">
+    <span class="input-group-btn">
+        <button type="button" class="btn btn-success">+</button>
+    </span>
+</div>
+```
--- bootstrap-number-input.js
+++ bootstrap-number-input.js
@@ -1,3 +1,4 @@
+
 /* ========================================================================
  * bootstrap-spin - v1.0
  * https://github.com/wpic/bootstrap-spin
@@ -18,79 +19,83 @@
  * limitations under the License.
  * ========================================================================
  */
-
 (function ($) {
-
-    $.fn.bootstrapNumber = function (options) {
-
+    "use strict";
+    $.fn.bootstrapNumber = function( options ) {
         var settings = $.extend({
-            upClass: 'default',
+            center: true,
             downClass: 'default',
-            center: true
+            downText: '-',
+            upClass: 'default',
+            upText: '+',
+            useStaticControls: false
         }, options);
 
-        return this.each(function (e) {
-            var self = $(this);
-            var clone = self.clone();
-
-            var min = self.attr('min');
-            var max = self.attr('max');
+        return this.each(function(e) {
+            var self = $(this),
+                clone = self.clone(),
+                min = self.attr('min'),
+                max = self.attr('max'),
+                step = Number(self.attr('step')) || 1;
 
             function setText(n) {
-                if ((min && n < min) || (max && n > max)) {
+                if (isNaN(n) || (min && n < min) || (max && n > max)) {
                     return false;
                 }
-
                 clone.focus().val(n);
+                clone.trigger('change');
                 return true;
             }
 
-            var group = $("<div class='input-group'></div>");
-            var down = $("<button type='button'>-</button>").attr('class', 'btn btn-' + settings.downClass).click(function () {
-                setText(parseInt(clone.val()) - 1);
-            });
-            var up = $("<button type='button'>+</button>").attr('class', 'btn btn-' + settings.upClass).click(function () {
-                setText(parseInt(clone.val()) + 1);
-            });
-            $("<span class='input-group-btn'></span>").append(down).appendTo(group);
-            clone.appendTo(group);
-            if (clone) {
-                clone.css('text-align', 'center');
-            }
-            $("<span class='input-group-btn'></span>").append(up).appendTo(group);
+            var group = (settings.useStaticControls)
+                ? $(this).parent('.input-group')
+                : $("<div class='input-group'></div>");
+            var down = (settings.useStaticControls)
+                ? $(this).prev().children('[type="button"]')
+                : $("<button type='button'>" + settings.downText + "</button>").addClass('btn btn-' + settings.downClass);
+            var up = (settings.useStaticControls)
+                ? $(this).next().children('[type="button"]')
+                : $("<button type='button'>" + settings.upText + "</button>").addClass('btn btn-' + settings.upClass);
 
-            // remove spins from original
-            clone.prop('type', 'text').keydown(function (e) {
+            up.on('click', function() {
+                setText(Number(clone.val() || clone.attr('value')) + step);
+            });
+            down.on('click', function() {
+                setText(Number(clone.val() || clone.attr('value')) - step);
+            });
+
+            if (!settings.useStaticControls) {
+                $("<span class='input-group-btn'></span>").append(down).appendTo(group);
+                clone.appendTo(group);
+                if(clone && settings.center) {
+                    clone.css('text-align', 'center');
+                }
+                $("<span class='input-group-btn'></span>").append(up).appendTo(group);
+            } else {
+                clone = group.find('.form-control');
+            }
+
+            clone.prop('type', 'text').on('keydown', function(e) {
                 if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
-					(e.keyCode == 65 && e.ctrlKey === true) ||
-					(e.keyCode >= 35 && e.keyCode <= 39)) {
+                    (e.keyCode == 65 && e.ctrlKey === true) ||
+                    (e.keyCode >= 35 && e.keyCode <= 39)) {
                     return;
                 }
                 if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
                     e.preventDefault();
                 }
 
-                var c = String.fromCharCode(e.which);
-                var n = parseInt(clone.val() + c);
+                var c = String.fromCharCode(e.which),
+                    n = Number(clone.val() + c);
 
-                //if ((min && n < min) || (max && n > max)) {
-                //    e.preventDefault();
-                //}
-            });
-
-            clone.prop('type', 'text').blur(function (e) {
-                var c = String.fromCharCode(e.which);
-                var n = parseInt(clone.val() + c);
-                if ((min && n < min)) {
-                    setText(min);
-                }
-                else if (max && n > max) {
-                    setText(max);
+                console.debug(c, n);
+                if ((min && n < min) || (max && n > max)) {
+                    e.preventDefault();
                 }
             });
-
-
-            self.replaceWith(group);
+            if (!settings.useStaticControls) {
+                self.replaceWith(group);
+            }
         });
     };
-}(jQuery));
+} (jQuery));
